09dee1378fe2c829a2223623394dfa3474c4aae0
wangsr2 <1040737853@qq.com>
2018-12-07 11:41:30 +0800

    ..

--- WebApplication1/Controllers/TestController.cs
+++ WebApplication1/Controllers/TestController.cs
@@ -25,7 +25,29 @@ namespace WebApplication1.Controllers
             ct.CustomerName = "bbb";
             ct.Adddress = "anaa";
             return ct;
-            
+        }
+        public ActionResult GetView()
+        {
+            //获取当前时间
+            //DateTime dt = DateTime.Now;
+
+            //获取当前小时数
+            //int hour = dt.Hour;
+            //根据小时数判断需要返回哪个视图
+            //if (hour > 12)
+            //{
+            //  return View("MyView");
+            //}
+            //else
+            //{
+            //    return View("YourView");
+            // }
+            Employee emp = new Employee();
+            emp.Name = "历史";
+            emp.Salary = 20002;
+            ViewData["EmpKey"] = emp;
+            return View("MyView");
+
         }
     }
 }
